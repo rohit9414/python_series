0fd1c35fda7102a06c8f77238ded91621250d8e8
rohit9414 <rohit6201164441@gmail.com>
2026-05-25 09:32:44 +0000

    numeric

--- d3.py
+++ d3.py
@@ -1,14 +1,15 @@
-#numeric data type
+# #numeric data type
 a=5
-print(type(a))
+# print(type(a))
 b=4.0
-print(type(b))
-c=(3+5j)
-print(complex(a,b))
-print(type(c))
-bool=True
-print(type(bool))
-print()
+# print(type(b))
+# c=(3+5j)
+# print(complex(a,b))
+# print(type(c))
+# bool=True
+# print(type(bool))
+print(a>4 and b<5)
+print(a>4 or b>5)
 
 
 
